c88d9bc21fc588300458a2c1cffee5a64901416a
aghabilal98 <129630047+aghabilal98@users.noreply.github.com>
2025-09-02 13:08:42 +0500

    game upload
    game upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "SmooGames-TikTok Live Games-6.1";
 const contentToCache = [
     "Build/tiktokTest.loader.js",
-    "Build/tiktokTest.framework.js.gz",
-    "Build/tiktokTest.data.gz",
-    "Build/tiktokTest.wasm.gz",
+    "Build/tiktokTest.framework.js.unityweb",
+    "Build/tiktokTest.data.unityweb",
+    "Build/tiktokTest.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
